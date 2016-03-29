4ad6eb34eb07a3c35e417517b07f0ccad9e66a3f
Evans-Munatsa <gagamel.em@gmail.com>
2016-03-29 14:49:49 +0200

    first hellojs

--- hello_joe.js
+++ hello_joe.js
@@ -1,3 +1,9 @@
-module.exports = function(){
-    
-};
+module.exports = function(username) {
+    if (username === "Xolani") {
+        return "Hello, " + username.toUpperCase() + '!';
+    } else if (!username) {
+        return "Hello, " + username.toUpperCase() + '!';
+    } else {
+        return "Hello!";
+    }
+}
